7b78c9a88acaa2aa20210918c648e3b3166f5b9f
Patient Padawan <1PatientPadawan@gmail.com>
2019-11-10 22:07:00 -0700

    minor styling fix and deployment

--- Noteful-CLIENT/src/config.js
+++ Noteful-CLIENT/src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://damp-eyrie-73298.herokuapp.com/',
+    API_ENDPOINT: 'https://damp-eyrie-73298.herokuapp.com',
     API_KEY: process.env.REACT_APP_API_KEY
 }
